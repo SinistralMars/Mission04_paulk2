109e7d366a2c9a9e262bd839b72772c8f8a95402
Kevin Paul <paulk2@byu.edu>
2023-01-30 19:09:30 -0700

    Add project files.

---
+++ Mission04_paulk2/wwwroot/js/GradeCalc.js
@@ -0,0 +1,36 @@
+﻿$('#btnSubmit').click(function () {
+
+    /*Get percentage from form*/
+    var percent = $('#numPercent').val()
+    var letter
+
+    // If else tree checking and assigning correct value for the grade to return to page
+    if (percent >= 94) {
+        letter = 'A'
+    } else if (percent >= 90) {
+        letter = 'A-'
+    } else if (percent >= 87) {
+        letter = 'B+'
+    } else if (percent >= 84) {
+        letter = 'B'
+    } else if (percent >= 80) {
+        letter = 'B-'
+    } else if (percent >= 77) {
+        letter = 'C+'
+    } else if (percent >= 74) {
+        letter = 'C'
+    } else if (percent >= 70) {
+        letter = 'C-'
+    } else if (percent >= 67) {
+        letter = 'D+'
+    } else if (percent >= 64) {
+        letter = 'D'
+    } else if (percent >= 60) {
+        letter = 'D-'
+    } else {
+        letter = 'E'
+    }
+
+    // Feeds correct letter to header in form
+    $('#gradeAnswer').html(letter)
+})
