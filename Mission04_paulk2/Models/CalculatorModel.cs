1313bc17da211959f314210c74921b5575663a47
Kevin Paul <paulk2@byu.edu>
2023-02-06 20:20:42 -0700

    Finished push

---
+++ Mission04_paulk2/Models/CalculatorModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_paulk2.Models
+{
+    public class CalculatorModel
+    {
+        [Range(0, 100)]
+        [Required]
+        public int Assignments { get; set; }
+
+        [Range(0, 100)]
+        [Required]
+        public int GroupProjects { get; set; }
+
+        [Range(0, 100)]
+        [Required]
+        public int Quizzes { get; set; }
+
+        [Range(0, 100)]
+        [Required]
+        public int Midterm { get; set; }
+
+        [Range(0, 100)]
+        [Required]
+        public int Final { get; set; }
+
+        [Range(0, 100)]
+        [Required]
+        public int Intex { get; set; }
+
+        public string LetterGrade { get; set; }
+    }
+}
